ef0e318cf6f80c83af52b0fa6e545d0cc5f8fafe
유나 <124111014+yuuunaaaa@users.noreply.github.com>
2023-03-24 17:55:35 +0900

    Add files via upload

---
+++ day09.sql
@@ -0,0 +1,15 @@
+CREATE TABLE multi.mock (
+	id varchar(100) NULL,
+	first_name varchar(200) NULL,
+	last_name varchar(200) NULL,
+	email varchar(200) NULL,
+	gender varchar(100) NULL,
+	address varchar(200) NULL
+)
+
+CREATE TABLE multi.mock2 (
+	id varchar(100) NULL,
+	model varchar(200) NULL,
+	color varchar(200) NULL,
+	money varchar(200) NULL
+)
